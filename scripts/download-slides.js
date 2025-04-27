dfb77dd9196a582bd68f851ede1219da324786a2
tp.jp <chronokreuz@gmail.com>
2025-04-27 22:05:05 +0900

    コメント追加

--- scripts/download-slides.js
+++ scripts/download-slides.js
@@ -14,32 +14,33 @@ async function downloadSlides() {
     url.trim()
   );
 
-  for (const url of slideUrls) {
-    const presentationId = url.match(/\/presentation\/d\/([a-zA-Z0-9-_]+)/)[1];
-    const presentation = await slides.presentations.get({ presentationId });
+  console.log(slideUrls);
+  // for (const url of slideUrls) {
+  //   const presentationId = url.match(/\/presentation\/d\/([a-zA-Z0-9-_]+)/)[1];
+  //   const presentation = await slides.presentations.get({ presentationId });
 
-    const tempDir = `${process.env.TEMP_DIR}/${presentation.data.title}`;
-    await fs.promises.mkdir(tempDir, { recursive: true });
+  //   const tempDir = `${process.env.TEMP_DIR}/${presentation.data.title}`;
+  //   await fs.promises.mkdir(tempDir, { recursive: true });
 
-    for (let i = 0; i < presentation.data.slides.length; i++) {
-      const response = await slides.presentations.pages.getThumbnail({
-        presentationId,
-        pageObjectId: presentation.data.slides[i].objectId,
-        thumbnailProperties: {
-          thumbnailSize: "LARGE",
-        },
-      });
+  //   for (let i = 0; i < presentation.data.slides.length; i++) {
+  //     const response = await slides.presentations.pages.getThumbnail({
+  //       presentationId,
+  //       pageObjectId: presentation.data.slides[i].objectId,
+  //       thumbnailProperties: {
+  //         thumbnailSize: "LARGE",
+  //       },
+  //     });
 
-      const imageUrl = response.data.contentUrl;
-      const imageResponse = await fetch(imageUrl);
-      const buffer = await imageResponse.buffer();
+  //     const imageUrl = response.data.contentUrl;
+  //     const imageResponse = await fetch(imageUrl);
+  //     const buffer = await imageResponse.buffer();
 
-      await fs.promises.writeFile(
-        `${tempDir}/slide-${(i + 1).toString().padStart(3, "0")}.png`,
-        buffer
-      );
-    }
-  }
+  //     await fs.promises.writeFile(
+  //       `${tempDir}/slide-${(i + 1).toString().padStart(3, "0")}.png`,
+  //       buffer
+  //     );
+  //   }
+  // }
 }
 
 downloadSlides().catch(console.error);
