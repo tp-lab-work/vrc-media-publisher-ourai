661bfb1a47a6a6d94b437528023232b4d3ff218a
tp.jp <chronokreuz@gmail.com>
2025-04-27 22:22:29 +0900

    デバッグコード削除

--- scripts/download-slides.js
+++ scripts/download-slides.js
@@ -13,37 +13,33 @@ async function downloadSlides() {
   const slideUrls = process.env.SLIDE_URLS.split("\n").filter((url) =>
     url.trim()
   );
-  console.log(slideUrls);
 
-  // デバッグ用にauth情報を確認
-  console.log("Slides API client created:", !!slides);
+  for (const url of slideUrls) {
+    const presentationId = url.match(/\/presentation\/d\/([a-zA-Z0-9-_]+)/)[1];
+    const presentation = await slides.presentations.get({ presentationId });
 
-  // for (const url of slideUrls) {
-  //   const presentationId = url.match(/\/presentation\/d\/([a-zA-Z0-9-_]+)/)[1];
-  //   const presentation = await slides.presentations.get({ presentationId });
+    const tempDir = `${process.env.TEMP_DIR}/${presentation.data.title}`;
+    await fs.promises.mkdir(tempDir, { recursive: true });
 
-  //   const tempDir = `${process.env.TEMP_DIR}/${presentation.data.title}`;
-  //   await fs.promises.mkdir(tempDir, { recursive: true });
+    for (let i = 0; i < presentation.data.slides.length; i++) {
+      const response = await slides.presentations.pages.getThumbnail({
+        presentationId,
+        pageObjectId: presentation.data.slides[i].objectId,
+        thumbnailProperties: {
+          thumbnailSize: "LARGE",
+        },
+      });
 
-  //   for (let i = 0; i < presentation.data.slides.length; i++) {
-  //     const response = await slides.presentations.pages.getThumbnail({
-  //       presentationId,
-  //       pageObjectId: presentation.data.slides[i].objectId,
-  //       thumbnailProperties: {
-  //         thumbnailSize: "LARGE",
-  //       },
-  //     });
+      const imageUrl = response.data.contentUrl;
+      const imageResponse = await fetch(imageUrl);
+      const buffer = await imageResponse.buffer();
 
-  //     const imageUrl = response.data.contentUrl;
-  //     const imageResponse = await fetch(imageUrl);
-  //     const buffer = await imageResponse.buffer();
-
-  //     await fs.promises.writeFile(
-  //       `${tempDir}/slide-${(i + 1).toString().padStart(3, "0")}.png`,
-  //       buffer
-  //     );
-  //   }
-  // }
+      await fs.promises.writeFile(
+        `${tempDir}/slide-${(i + 1).toString().padStart(3, "0")}.png`,
+        buffer
+      );
+    }
+  }
 }
 
-downloadSlides().catch(console.error);
+downloadSlides();
